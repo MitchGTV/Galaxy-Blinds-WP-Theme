245d1ebac631ed827decf1bb5020b6d9d337db49
Mitchell Butterfield <mitchellcann98@gmail.com>
2024-03-28 15:59:57 +0000

    Update bloginfo with esc_url

--- functions.php
+++ functions.php
@@ -4,7 +4,6 @@
 
 	// Load jQuery
     function register_my_scripts(){
-        wp_deregister_script('jquery');
         wp_register_script('jquery', ("https://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js"), false);
         wp_enqueue_script('jquery');
     }
